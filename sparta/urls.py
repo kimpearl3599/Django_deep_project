627c01f305dbd8e90a102943372dbe0933e83864
kimpearl3599 <kimpearl3599@gmail.com>
2022-02-14 10:31:06 +0900

    isort

--- sparta/urls.py
+++ sparta/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from typing import Dict
+
 from django.contrib import admin
 from django.http import HttpRequest
 from django.urls import path

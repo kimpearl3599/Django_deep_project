5d186ed15408edac1f2875e0b48f86632eeababe
kimpearl3599 <kimpearl3599@gmail.com>
2022-02-15 17:35:18 +0900

    like service request 7

--- tabom/services/article_service.py
+++ tabom/services/article_service.py
@@ -1,5 +1,12 @@
+
+from django.db.models import QuerySet
+
 from tabom.models import Article
 
 
 def get_an_article(article_id: int) -> Article:
     return Article.objects.filter(id=article_id).get()
+
+
+def get_article_list(offset: int, limit: int) -> QuerySet[Article]:
+    return Article.objects.order_by("-id")[offset: offset + limit]

8146eab44b80deaa39c54d844c07646aac9da572
kimpearl3599 <kimpearl3599@gmail.com>
2022-02-15 12:04:36 +0900

    like service

---
+++ tabom/tests/test_like_service.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+
+from tabom.models.article import Article
+from tabom.models.user import User
+from tabom.services.like_service import do_like
+
+
+class TestLikeService(TestCase):
+    def test_a_user_can_like_an_article(self) -> None:
+        # Given
+        user = User.objects.create(name="test")
+        article = Article.objects.create(title="test_title")
+
+        # When
+        like = do_like(user.id, article.id)
+
+        # Then
+        self.assertIsNotNone(like.id)
+        self.assertEqual(user.id, like.user_id)
+        self.assertEqual(article.id, like.article_id)
+
+    def test_a_user_can_like_on_article_only_once(self) -> None:
+        #Given
+        user = User.objects.create(name="test")
+        article = Article.objects.create(title="test_title")
+
+        #Expect
+        like1 = do_like(user.id, article.id)
+        with self.assertRaises(Exception):
+            pass
+            # raise Exception()
+        like2 = do_like(user.id, article.id)
+

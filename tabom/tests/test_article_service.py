c4274bf3fc106a99e29caa007f9f0041be256a83
kimpearl3599 <kimpearl3599@gmail.com>
2022-02-15 20:34:27 +0900

    like serviece request 8

--- tabom/tests/test_article_service.py
+++ tabom/tests/test_article_service.py
@@ -1,4 +1,6 @@
+from django.db import connection
 from django.test import TestCase
+from django.test.utils import CaptureQueriesContext
 
 from tabom.models import User
 from tabom.models.article import Article
@@ -28,20 +30,35 @@ class TestArticleService(TestCase):
             get_an_article(invalid_article_id)
 
     def test_get_article_list_should_prefetch_like(self) -> None:
-        #Given
+        # Given
         user = User.objects.create(name="test_user")
-        articles = [Article.objects.create(title=f"{i}") for i in range(1,21)]
+        articles = [Article.objects.create(title=f"{i}") for i in range(1, 21)]
         do_like(user.id, articles[-1].id)
 
-        #When
-        result_articles = get_article_list(0,10)
+        # When
+        with CaptureQueriesContext(connection) as ctx:
+            with self.assertNumQueries(3):
+                result_articles = get_article_list(user.id, 0, 10)
+                result_counts = [a.like_set.count() for a in result_articles]
 
-        #Then
-        self.assertEqual(len(result_articles), 10)
-        self.assertEqual(1, result_articles[0].like_set.count())
-        self.assertEqual(
-            [a.id for a in reversed(articles[10:21])],
-            [a.id for a in result_articles],
-        )
+                # Then
+                self.assertEqual(len(result_articles), 10)
+                self.assertEqual(1, result_counts[0])
+                self.assertEqual(
+                    [a.id for a in reversed(articles[10:21])],
+                    [a.id for a in result_articles],
+                )
 
+    def test_get_article_list_should_contain_my_like_when_like_exists(self) -> None:
+        # Given
+        user = User.objects.create(name="test_user")
+        article1 = Article.objects.create(title="artice1")
+        like = do_like(user.id, article1.id)
+        Article.objects.create(title="article2")
 
+        # When
+        articles = get_article_list(user.id, 0, 10)
+
+        # Then
+        self.assertEqual(like.id, articles[1].my_likes[0].id)
+        self.assertEqual(0, len(articles[0].my_likes))

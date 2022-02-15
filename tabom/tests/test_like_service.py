6e5544f6469c724798d4f2b9af07751c54ff88fb
kimpearl3599 <kimpearl3599@gmail.com>
2022-02-15 16:35:36 +0900

    like service request 4~6

--- tabom/tests/test_like_service.py
+++ tabom/tests/test_like_service.py
@@ -2,8 +2,9 @@ from django.db import IntegrityError
 from django.test import TestCase
 
 from tabom.models.article import Article
+from tabom.models.like import Like
 from tabom.models.user import User
-from tabom.services.like_service import do_like
+from tabom.services.like_service import do_like, undo_like
 
 
 class TestLikeService(TestCase):
@@ -59,3 +60,16 @@ class TestLikeService(TestCase):
         # Then
         article = Article.objects.get(id=article.id)
         self.assertEqual(1, article.like_set.count())
+
+    def test_a_use_can_undo_like(self) -> None:
+        # Given
+        user = User.objects.create(name="test")
+        article = Article.objects.create(title="test_title")
+        like = do_like(user.id, article.id)
+
+        # When
+        undo_like(user.id, article.id)
+
+        # Then
+        with self.assertRaises(Like.DoesNotExist):
+            Like.objects.filter(id=like.id).get()
